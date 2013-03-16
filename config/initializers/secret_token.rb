bbfb6e442a37819295ee18897bcb9ab10cbe5a84
Jonathan Twaddell <jon@arclocal.com>
2013-03-15 20:17:08 -0700

    updated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alijosapp::Application.config.secret_token = '68d6f81c3f4b4d9547dda27f7c29bd9337063dee9193b1d41070e201663c3b2adfe118746a6aaff6fd1ef6b1a18a8805199b3b2391512baf3b9c5a805400c903'
